9cedb2cb1c853f1c1fed305ee0512608f42172ba
kienpro307 <kien3072003@gmail.com>
2025-07-09 15:01:50 +0700

    hide naviation bar

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.os.Build;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -33,6 +35,31 @@ public class SplashScreen {
                     mSplashDialog = new Dialog(activity, themeResId);
                     mSplashDialog.setContentView(R.layout.launch_screen);
                     mSplashDialog.setCancelable(false);
+
+                    if (mSplashDialog.getWindow() != null) {
+                        View decorView = mSplashDialog.getWindow().getDecorView();
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            mSplashDialog.getWindow().setDecorFitsSystemWindows(false);
+                            decorView.getWindowInsetsController().hide(
+                                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars()
+                            );
+                            decorView.getWindowInsetsController().setSystemBarsBehavior(
+                                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                            );
+                        } else {
+                            // fallback cho Android < R
+                            @SuppressWarnings("deprecation")
+                            int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                            decorView.setSystemUiVisibility(flags);
+                        }
+                    }
+
                     if (fullScreen) {
                         setActivityAndroidP(mSplashDialog);
                     }
